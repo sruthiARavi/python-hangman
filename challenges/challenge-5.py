2bddc28f01a8d3bcb0fe9feb422ebf3a7a00ab85
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-09 00:18:39 -0400

    Update challenge-5.py

--- challenges/challenge-5.py
+++ challenges/challenge-5.py
@@ -1,26 +1,27 @@
-#Step 4
-
-import random
+import hangman_logo
 import hangman_art
+import random
 
 lives = 6
 end_of_game = False
-#word_list = ["ardvark", "baboon", "camel"]
-#chosen_word = random.choice(word_list)
 chosen_word = ""
+previous_guesses = []
 
 #choose word from wordlist.txt which is in the same directory as this file
 with open('wordlist.txt') as f:
     #For my reference : 
     #with is statement is used to wrap the execution of a block with methods defined by a context manager (an object that defines the runtime context to be established when executing a with statement)
     #If you want to read all the lines of a file in a list you can also use list(f) or f.readlines().
-    chosen_word = random.choice(f.readlines())
+    #Using strip() function because of extra spaces
+    chosen_word = random.choice(f.readlines()).strip()
     
 word_length = len(chosen_word)
 
+#print the logo first
+print(hangman_logo.logo)
 
 #Testing code
-#print(f'Pssst, the solution is {chosen_word}.')
+print(f'Pssst, the solution is {chosen_word}. Length is {word_length}\n')
 
 #Create blanks
 display = []
@@ -31,33 +32,39 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter        
-                
-    #If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
-    if guess not in chosen_word:
-            print("Incorrect guess")
-            lives -= 1                        
-            if lives == 0:
-                end_of_game = True
-                print("You lose.\n")
-                
-                
+    if guess in previous_guesses:
+        print(f"You've already guessed {guess}")
+        
+    #If guess is not a letter in the chosen_word, then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."  
+    elif guess not in chosen_word:        
+        print(f"You guessed {guess}. That's not in the word. You lose a life.")
+        previous_guesses.append(guess)
+        lives -= 1                        
+        if lives == 0:
+            end_of_game = True
+            print("********************* YOU LOSE *********************\n")
+            
+    else:
+        previous_guesses.append(guess)
+        for position in range(word_length):
+            letter = chosen_word[position]
+            # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter        
+                                                       
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
     #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win.")
+        print("********************* YOU WIN *********************")
 
     #Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(hangman_art.stages[lives])
+    print(f"*************{lives}/6 lives left*************")
     if lives == 0:
         print(f"\nThe word is {chosen_word}")
-    print(" ")
+        
+    print("\n")
